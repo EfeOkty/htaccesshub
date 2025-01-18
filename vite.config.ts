ada21d4864aaebb1bbed12828f305119d214e10e
efeokty <efeoktay59@gmail.com>
2025-01-18 05:09:48 +0300

    Asset yolları düzeltildi

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import { ViteImageOptimizer } from 'vite-plugin-image-optimizer'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/htaccesshub/',
+  base: './',
   plugins: [
     react(),
     visualizer({
