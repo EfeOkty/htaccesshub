49438643fc7a5d9b76c281ec9626510471bb28d7
efeokty <efeoktay59@gmail.com>
2025-01-18 06:06:54 +0300

    fix: GitHub Pages SPA routing and path fixes

---
+++ vite.config.js
@@ -0,0 +1,40 @@
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import { ViteImageOptimizer } from 'vite-plugin-image-optimizer';
+export default defineConfig({
+    base: '/htaccesshub/',
+    plugins: [
+        react(),
+        ViteImageOptimizer({
+            png: { quality: 70 },
+            jpg: { quality: 70 },
+            jpeg: { quality: 70 },
+            svg: {
+                multipass: true,
+                plugins: [
+                    {
+                        name: 'preset-default',
+                        params: {
+                            overrides: {
+                                removeViewBox: false,
+                                removeTitle: false,
+                            },
+                        },
+                    },
+                ],
+            },
+        }),
+    ],
+    build: {
+        outDir: 'dist',
+        emptyOutDir: true,
+        assetsDir: '',
+        rollupOptions: {
+            output: {
+                entryFileNames: '[name].[hash].js',
+                chunkFileNames: '[name].[hash].js',
+                assetFileNames: '[name].[ext]'
+            }
+        }
+    },
+});
